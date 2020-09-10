679bd65cbd39bc3639d6396fced377df90015224
Princess <wilson.princess@yahoo.ie>
2020-09-10 02:32:26 +0100

    creating short unique id

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
 from flask import Flask, request, redirect, render_template, url_for
 from flask_mysqldb import MySQL
+import shortuuid
 from config import app
 
-# make an instance of flask class
-# creates an instance which will provide us the access
+
 mysql = MySQL(app)
 
 
-# routing - connecting a url to a python function
 @app.route('/', methods=['GET', 'POST'])  # decorator
 def index():
-    if request.method == "POST":
-        # fetches the data from form and puts it into new variable -url
-        url = request.form['url-link']
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO urls(url) VALUES (%s)", [url])
-        mysql.connection.commit()
-        cur.close()
-        return 'success'
-    # request.method - tells us whether GET or POST is used
     return render_template("index.html")
+
+
+@app.route('/<url_id>')
+def url_id(url_id):
+    cur = mysql.connection.cursor()
+    check_url = cur.execute("SELECT url FROM shorturl WHERE shortuuid=%s",url_id)
+    if check_url:
+        return redirect(check_url.url)
+    else:
+        return "wrong url"
+
+
+@app.route('/processor', methods=['POST'])  # decorator
+def pro():
+    urlid = shortuuid.ShortUUID().random(length=4)
+    url = request.form['url-link']
+    new_link = urlid
+    cur = mysql.connection.cursor()
+    cur.execute(
+        "INSERT INTO shorturl(url,shortuuid) VALUES (%s,%s)", [url, urlid])
+    mysql.connection.commit()
+    return 'http://127.0.0.1:5000/'+new_link
 
 
 if __name__ == '__main__':
