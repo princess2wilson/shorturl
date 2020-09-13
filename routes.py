2d645a9dbb3896561bb6cb9c4e6bdcc16d9a2db1
Princess <wilson.princess@yahoo.ie>
2020-09-13 16:19:51 +0100

    copy to clipboard functionality

--- routes.py
+++ routes.py
@@ -2,7 +2,7 @@ from flask import Flask, request, redirect, render_template, url_for
 from flask_mysqldb import MySQL
 import shortuuid
 from config import app
-from insert_get_url import get_short_url_row, insert_url
+from models import get_short_url_row, insert_url
 
 
 mysql = MySQL(app)
@@ -19,12 +19,12 @@ def url_id(url_id):
     if row:
         return redirect(row[1])
     else:
-        return "wrong url"
+        return "This url is invalid"
 
 
 @ app.route('/short-url', methods=['POST'])  # need to change this name ??
 def short_url():
-    urlid = shortuuid.ShortUUID().random(length=4)
+    urlid = shortuuid.ShortUUID().random(length=5)
     url = request.form['url-link']
     insert_url(url, urlid)
     new_url = 'http://short-ly/'+urlid
