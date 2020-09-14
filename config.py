ed0f942fef9404bbfb4d36ef58b54c177478b3cd
Princess <wilson.princess@yahoo.ie>
2020-09-14 19:59:57 +0100

    database changes

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ from flask_mysqldb import MySQL
 app = Flask(__name__)
 
 # db configuration required for our Flask app
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'password'
-app.config['MYSQL_DB'] = 'flaskapp'
+app.config['MYSQL_HOST'] = ''
+app.config['MYSQL_USER'] = ''
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = ''
 
 # creates an instance which will provide us the access
